10e026424af1134bfc651c8ed12dcd44dceb8298
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-16 19:39:56 +0000

    Create Sign_up.jsx

---
+++ src/components/sign_up/Sign_up.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Username" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
