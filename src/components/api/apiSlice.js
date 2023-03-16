3888e16e7653dae2598a34ab37104e197b7b1155
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-16 19:20:44 +0000

    Update apiSlice.js

--- src/components/api/apiSlice.js
+++ src/components/api/apiSlice.js
@@ -3,13 +3,34 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const stakeConnectApi = createApi({
   reducerPath: 'stakeHolders',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3000/' }),
-  tagTypes: ['Administrator'],
   endpoints: (builder) => ({
+    signInAdministrator: builder.mutation({
+      query: (body) => ({
+        url: 'administrators/sign_in',
+        method: 'POST',
+        body,
+      }),
+      transformResponse: (response, meta) => {
+        const token = meta.response.headers.get('Authorization');
+        return {token, ...response.status};
+      },
+    }),
     getAdministrators: builder.query({
       query: () => 'administrators',
-      providesTags: ['Administrator'],
+    }),
+    signOutAdministrator: builder.mutation({
+      query: ({body, headers}) => ({
+        url: 'administrators/sign_out',
+        method: 'DELETE',
+        body,
+        headers,
+      }),
     }),
   }),
 });
 
-export const { useGetAdministratorsQuery } = stakeConnectApi;
+export const {
+  useSignInAdministratorMutation,
+  useGetAdministratorsQuery,
+  useSignOutAdministratorMutation
+} = stakeConnectApi;
