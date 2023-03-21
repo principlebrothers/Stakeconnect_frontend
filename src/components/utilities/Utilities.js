8229cd03feeac98ad23127d1f6eebdb15db5b5d0
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-21 21:08:05 +0000

    Update Utilities.js

--- src/components/utilities/Utilities.js
+++ src/components/utilities/Utilities.js
@@ -1,3 +1,10 @@
+import { IoLogOutOutline } from 'react-icons/io5';
+import { SiGoogleclassroom, SiAboutdotme } from 'react-icons/si';
+import { GiTeacher, GiGuards } from 'react-icons/gi';
+import { RiParentFill } from 'react-icons/ri';
+import { TbCalendarEvent } from 'react-icons/tb';
+import { IoMdSettings, IoMdHelpCircle } from 'react-icons/io';
+
 export function setToken(response) {
   localStorage.setItem('token', response);
 }
@@ -9,3 +16,27 @@ export function getToken() {
 export function clearToken() {
   localStorage.removeItem('token');
 }
+
+export const menus = [
+  'Grades',
+  'Teachers',
+  'Students',
+  'Parents',
+  'Events',
+  'Settings',
+  'Help',
+  'About',
+  'Logout',
+];
+
+export const icons = [
+  SiGoogleclassroom,
+  GiTeacher,
+  RiParentFill,
+  GiGuards,
+  TbCalendarEvent,
+  IoMdSettings,
+  IoMdHelpCircle,
+  SiAboutdotme,
+  IoLogOutOutline,
+];
