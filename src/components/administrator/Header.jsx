4e13ee1588d7372ada12ebff323236a1b907bf74
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-24 11:50:28 +0000

    Update Header.jsx

--- src/components/administrator/Header.jsx
+++ src/components/administrator/Header.jsx
@@ -13,13 +13,11 @@ function Header({ image, name, email }) {
         </button>
       </form>
       <div
-        className={styles.user__details + ' flex center a__rem__gap inner__gap'}
+        className={styles.user__details}
       >
         <IoNotificationsSharp className={styles.notice} />
         <img
-          src={
-            'https://th.bing.com/th/id/R.a0045d5d3a218f82448ad164cb7a5e7a?rik=US3eWTCrN1gi2A&riu=http%3a%2f%2fst1.bgr.in%2fwp-content%2fuploads%2f2016%2f09%2felon-musk-space-x-getty.jpg&ehk=fFBYPE%2fw47EkU9E9asbbZM5WBYqthAhP7VrUwRI9Vmk%3d&risl=&pid=ImgRaw&r=0'
-          }
+          src={image}
           alt='user'
           className={styles.img}
           width='45px'
