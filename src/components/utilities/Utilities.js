a6b2ac87b89058aac65fa5026a94ce97eaa23719
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-24 23:29:45 +0000

    Add logout functionality and update exiting data

--- src/components/utilities/Utilities.js
+++ src/components/utilities/Utilities.js
@@ -1,9 +1,32 @@
-import { IoLogOutOutline } from 'react-icons/io5';
 import { SiGoogleclassroom, SiAboutdotme } from 'react-icons/si';
 import { GiTeacher, GiGuards } from 'react-icons/gi';
 import { RiParentFill } from 'react-icons/ri';
 import { TbCalendarEvent } from 'react-icons/tb';
 import { IoMdSettings, IoMdHelpCircle } from 'react-icons/io';
+import { RxDashboard } from 'react-icons/rx';
+
+import { useNavigate } from 'react-router-dom';
+import { useSignOutAdministratorMutation } from '../api/apiSlice';
+
+export function useLogOut() {
+  const navigate = useNavigate();
+  const [signOutAdministrator, { isLoading }] =
+    useSignOutAdministratorMutation();
+
+  const handleLogOut = async () => {
+    const token = getToken();
+    await signOutAdministrator({ headers: { Authorization: token } })
+      .unwrap()
+      .then(() => {
+        clearToken();
+        navigate('/login');
+      })
+      .catch((error) => error.message);
+  };
+
+  return { handleLogOut, isLoading };
+}
+
 
 export function setToken(response) {
   localStorage.setItem('token', response);
@@ -18,18 +41,47 @@ export function clearToken() {
 }
 
 export const menus = [
-  'Grades',
-  'Teachers',
-  'Students',
-  'Parents',
-  'Events',
-  'Settings',
-  'Help',
-  'About',
-  'Logout',
+  { id: 'dashboard', path: '/dashboard', page: 'Dashboard' },
+  { id: 'grades', path: '/grades', page: 'Grades' },
+  {
+    id: 'teachers',
+    path: '/teachers',
+    page: 'Teachers',
+  },
+  {
+    id: 'students',
+    path: '/students',
+    page: 'Students',
+  },
+  {
+    id: 'parents',
+    path: '/parents',
+    page: 'Parents',
+  },
+  {
+    id: 'events',
+    path: '/events',
+    page: 'Events',
+  },
+  {
+    id: 'settings',
+    path: '/settings',
+    page: 'Settings',
+  },
+  {
+    id: 'help',
+    path: '/help',
+    page: 'Help',
+  },
+  {
+    id: 'about',
+    path: '/about',
+    page: 'About',
+  },
 ];
 
 export const icons = [
+  RxDashboard,
   SiGoogleclassroom,
   GiTeacher,
   RiParentFill,
@@ -38,5 +90,4 @@ export const icons = [
   IoMdSettings,
   IoMdHelpCircle,
   SiAboutdotme,
-  IoLogOutOutline,
 ];
