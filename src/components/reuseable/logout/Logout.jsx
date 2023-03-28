467be9cd5fdf24b4cf2700126cae155365a7468a
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-28 23:22:57 +0000

    Update Logout.jsx

--- src/components/reuseable/logout/Logout.jsx
+++ src/components/reuseable/logout/Logout.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import { useLogOut } from '../../utilities/Utilities';
-
 import { IoLogOutOutline } from 'react-icons/io5';
+import { useLogOut } from '../../utilities/Utilities';
+import Modal from '../model/Modal';
+
 import styles from './Logout.module.css';
 
 function Logout() {
-  const { handleLogOut, isLoading } = useLogOut();
+  const {
+    handleLogOut, isLoading, modal, setModal,
+  } = useLogOut();
 
   return (
-    <button
-      type='button'
-      className={styles.log__out}
-      disabled={isLoading}
-      onClick={handleLogOut}
-    >
-      {isLoading ? 'logging out...' : <IoLogOutOutline className={styles.logout__sign} />}
-    </button>
+    <>
+      <button
+        type="button"
+        className={styles.log__out}
+        disabled={isLoading}
+        onClick={handleLogOut}
+      >
+        {isLoading ? (
+          'logging out...'
+        ) : (
+          <IoLogOutOutline className={styles.logout__sign} />
+        )}
+      </button>
+      {modal.isError && (
+        <Modal
+          message={modal.message}
+          type={modal.type}
+          onClose={() => setModal({ isError: false, message: '', type: '' })}
+        />
+      )}
+    </>
   );
 }
 
-export default Logout
+export default Logout;
